faec5ba23dd8faf3fc457793acea3bb725682c98
vincent-uden <vincent.uden@colmeo.se>
2024-03-05 11:58:01 +0100

    Env parsin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use axum::{
     routing::get,
     Router,
@@ -6,10 +8,24 @@ use axum::{
 mod index;
 
 use index::index;
+use serde::Deserialize;
 use tower_http::services::ServeDir;
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    git_root: PathBuf,
+}
 
 #[tokio::main]
 async fn main() {
+    dotenv::dotenv().ok();
+    let config = match envy::from_env::<Config>() {
+        Ok(c) => c,
+        Err(e) => {
+            panic!("Error parsing .env file\n{}", e);
+        }
+    };
+
     let serve_dir = ServeDir::new("assets");
 
     let app = Router::new().route("/", get(index)).fallback_service(serve_dir);
